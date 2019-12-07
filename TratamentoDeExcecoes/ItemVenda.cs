9b0bd2437bd574da37ba0e7766238c7f9db47fa7
Romulo-Reis <romuloreistavares@hotmail.com>
2019-12-07 10:57:35 -0300

    Primeiro commit do projeto.

---
+++ TratamentoDeExcecoes/ItemVenda.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TratamentoDeExcecoes
+{
+    class ItemVenda
+    {
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public decimal Quantidade { get; set; }
+    }
+}
